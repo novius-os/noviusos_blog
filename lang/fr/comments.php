2f971a42f81fbd97de9fca3b7c4c22f2719b52a8
Sébastien Drouyer <drouyer@novius.com>
2012-04-18 17:02:00 +0200

    adding comment in language...

--- lang/fr/comments.php
+++ lang/fr/comments.php
@@ -1,12 +1,13 @@
 <?php
 return array(
-    'Posted: '              => 'Posté par : ',
-    ':comments comments'   => ':comments commentaires',
-    '1 comment' => '1 commentaire',
-    'Comment by :author' => 'Commentaire de :author',
-    'Tags: ' => 'Tags : ',
-    'Leave your comment:' => 'Laissez votre commentaire : ',
-    'Name:' => 'Nom : ',
-    'Email (not published):' => 'Email (non publié) : ',
-    'Your comment:' => 'Votre commentaire : '
+    'Posted: '                  => 'Posté par : ',
+    ':comments comments'        => ':comments commentaires',
+    '1 comment'                 => '1 commentaire',
+    'Comment by :author'        => 'Commentaire de :author',
+    'Tags: '                    => 'Tags : ',
+    'Leave your comment:'       => 'Laissez votre commentaire :',
+    'Name:'                     => 'Nom : ',
+    'Email (not published):'    => 'Email (non publié) : ',
+    'Your comment:'             => 'Votre commentaire : ',
+    'No comments'               => 'Pas de commentaire'
 );
