339d46b4bc92e8c0d205cb9bac2cb7ba3eecc6a1
Sébastien Drouyer <drouyer@novius.com>
2012-04-23 16:35:16 +0200

    improving preview...

--- views/preview.php
+++ views/preview.php
@@ -1,5 +1,4 @@
 <div style="overflow: hidden">
     <img style="display: block; float: left; width: 64px; height: 64px;" src="static/apps/noviusos_blog/img/64/blog.png" />
     <h1 style="margin-left: 80px;"><?= __('Blog') ?></h1>
-</table>
 </div>
