1865d9b875718db5b19a809cd7c9b062b9239c6f
Sébastien Drouyer <drouyer@novius.com>
2012-05-21 15:15:49 +0200

    adding translations...

--- lang/fr/comments.php
+++ lang/fr/comments.php
@@ -1,13 +1,15 @@
 <?php
 return array(
-    'Posted: '                  => 'Posté le : ',
-    ':comments comments'        => ':comments commentaires',
-    '1 comment'                 => '1 commentaire',
-    'Comment by :author'        => 'Commentaire de :author',
-    'Tags: '                    => 'Tags : ',
-    'Leave your comment:'       => 'Laissez votre commentaire :',
-    'Name:'                     => 'Nom : ',
-    'Email (not published):'    => 'Email (non publié) : ',
-    'Your comment:'             => 'Votre commentaire : ',
-    'No comments'               => 'Pas de commentaire'
+    'Posted: '                                      => 'Posté le : ',
+    ':comments comments'                            => ':comments commentaires',
+    '1 comment'                                     => '1 commentaire',
+    'Comment by :author'                            => 'Commentaire de :author',
+    'Tags: '                                        => 'Tags : ',
+    'Leave your comment:'                           => 'Laissez votre commentaire :',
+    'Name:'                                         => 'Nom : ',
+    'Email (not published):'                        => 'Email (non publié) : ',
+    'Your comment:'                                 => 'Votre commentaire : ',
+    'No comments'                                   => 'Pas de commentaire',
+    'Captcha was incorrect !'                       => 'Le captcha entré est invalide !',
+    'Your comment has been successfully added !'    => 'Votre commentaire a été ajouté avec succès !'
 );
