e8ce0fd15a04aba8a104ac3e77847183731aa5d0
esperat <esperat@novius.fr>
2012-07-03 18:22:07 +0200

    Form to change default data in the silo / nugget

--- config/model/blog.config.php
+++ config/model/blog.config.php
@@ -6,15 +6,20 @@ return array(
                 'value' => 'blog_title',
                 'useTitle' => __('Title'),
             ),
-            /*\Nos\DataCatcher::TYPE_URL => array(
-                'value' => function($page) {
-                    return $page->get_href(array('absolute' => true));
+            \Nos\DataCatcher::TYPE_URL => array(
+                'value' => function($blog) {
+                    return $blog->first_url();
                 },
-                'options' => function($page) {
-                    return array($page->get_href(array('absolute' => true)));
+                'options' => function($blog) {
+                    $urls = array();
+                    foreach ($blog->_possible_url() as $possible)
+                    {
+                        $urls[$possible['page_id'].'::'.$possible['itemUrl']] = $possible['url'];
+                    }
+                    return $urls;
                 },
                 'useTitle' => __('Url'),
-            )*/
+            ),
         ),
     ),
 );
