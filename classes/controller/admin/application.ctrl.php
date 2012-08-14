78b05914ec456b2b26f4ed76af6553d8cc300490
esperat <esperat@novius.fr>
2012-08-14 11:46:23 +0200

    Removing errors

---
+++ classes/controller/admin/application.ctrl.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nos\BlogNews\Blog;
+
+class Controller_Admin_Application extends \Nos\BlogNews\Controller_Admin_Application
+{
+
+}
--- config/model/post.config.php
+++ config/model/post.config.php
@@ -4,23 +4,29 @@ return array(
         'Nos\Orm_Behaviour_Sharable' => array(
             'data' => array(
                 \Nos\DataCatcher::TYPE_TITLE => array(
-                    'value' => 'blog_title',
+                    'value' => 'post_title',
                     'useTitle' => __('Title'),
                 ),
-                /*\Nos\DataCatcher::TYPE_URL => array(
-                    'value' => function($blog) {
-                        return $blog->first_url();
+                \Nos\DataCatcher::TYPE_URL => array(
+                    'value' => function($post) {
+                        return $post->url_canonical();
                     },
-                    'options' => function($blog) {
+                    'options' => function($post) {
                         $urls = array();
-                        foreach ($blog->_possible_url() as $possible)
+                        foreach ($post->urls() as $possible)
                         {
                             $urls[$possible['page_id'].'::'.$possible['itemUrl']] = $possible['url'];
                         }
                         return $urls;
                     },
                     'useTitle' => __('Url'),
-                )*/
+                ),
+                \Nos\DataCatcher::TYPE_TEXT => array(
+                    'value' => function($post) {
+                        return $post->post_summary;
+                    },
+                    'useTitle' => __('Description'),
+                ),
             ),
             'data_catchers' => array(
                 array(
