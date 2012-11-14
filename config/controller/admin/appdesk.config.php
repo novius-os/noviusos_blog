a38e57f23b2b269bc99d7c7f39476974a735b246
Sébastien Drouyer <drouyer@novius.com>
2012-11-14 15:51:40 +0100

    cleaning config files

--- config/controller/admin/appdesk.config.php
+++ config/controller/admin/appdesk.config.php
@@ -8,27 +8,5 @@
  * @link http://www.novius-os.org
  */
 
-/*
-$config = array(
-    'query' => array(
-        'model' => '\Nos\BlogNews\Blog\Model_Post',
-        'order_by' => array('post_created_at' => 'DESC'),
-        'limit' => 20,
-    ),
-    'search_text' => 'post_title',
-    'views' => array(
-        'default' => array(
-            'name' => __('Default view'),
-            'json' => array('static/apps/noviusos_blog/js/admin/blog.js', 'static/apps/noviusos_blognews/js/admin/blognews.js'),
-        ),
-    ),
-);
-$appdesk = array(
-    'tab' => array(
-        'label' => __('Blog'),
-    ),
-);
-*/
-
 $base = \Config::load('noviusos_blognews::controller/admin/appdesk', true);
 return $base;
--- config/controller/admin/category.config.php
+++ config/controller/admin/category.config.php
@@ -1,12 +1,3 @@
 <?php
 $base = \Config::load('noviusos_blognews::controller/admin/category', true);
 return $base;
-/*
-return array(
-    'controller_url'  => 'admin/noviusos_blog/category',
-    'model' => 'Nos\\BlogNews\\Blog\\Model_Category',
-    'tab' => array(
-        'iconUrl' => 'static/apps/noviusos_blog/img/blog-16.png',
-    )
-);
-*/
