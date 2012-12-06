4dcee17a5ecafd37efbbcca783a8aa9398c59fe8
esperat <esperat@novius.fr>
2012-12-06 18:02:18 +0100

    "Translations cleanup"

--- config/common/category.config.php
+++ config/common/category.config.php
@@ -1,3 +1,8 @@
 <?php
 $base = \Config::load('noviusos_blognews::common/category', true);
+$base['actions'] = array(
+    'Nos\\BlogNews\\Blog\\Model_Category.add' => array(
+        'label' => __('Add a category'),
+    ),
+);
 return $base;
--- config/common/post.config.php
+++ config/common/post.config.php
@@ -1,3 +1,16 @@
 <?php
 $base = \Config::load('noviusos_blognews::common/post', true);
+
+$base['actions'] = array(
+    'Nos\BlogNews\Blog\Model_Post.add' => array(
+        'label' => __('Add a post'),
+    ),
+    'Nos\BlogNews\Blog\Model_Post.edit' => array(
+        'label' => __('Edit this post'),
+    ),
+    'Nos\BlogNews\Blog\Model_Post.delete' => array(
+        'label' => __('Delete this post'),
+    ),
+);
+
 return $base;
