406ab4a21006ed866335bb91405b5162e471da7b
Julian Esperat <esperat@novius.fr>
2013-01-30 15:49:01 +0100

    "Updated translations"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,72 +1,76 @@
 <?php
 
-// Generated on 23/01/2013 11:25:53
+// Generated on 29/01/2013 19:08:29
 
-// 21 out of 21 messages are translated (100%).
-// 81 out of 81 words are translated (100%).
+// 20 out of 20 messages are translated (100%).
+// 80 out of 80 words are translated (100%).
 
 return array(
-    #: config/controller/admin/appdesk.config.php:14
+    #: config/controller/admin/appdesk.config.php:16
     'post' => 'billet',
 
-    #: config/controller/admin/appdesk.config.php:15
+    #: config/controller/admin/appdesk.config.php:17
     'posts' => 'billets',
 
-    #: config/controller/admin/appdesk.config.php:16
+    #: config/controller/admin/appdesk.config.php:18
     'Showing {{x}} posts out of {{y}}' => '{{x}} billets sur {{y}} affichés',
 
-    #: config/controller/admin/appdesk.config.php:17
+    #: config/controller/admin/appdesk.config.php:19
     'Showing 1 post' => '1 billet affiché',
 
-    #: config/controller/admin/appdesk.config.php:18
+    #: config/controller/admin/appdesk.config.php:20
     'No posts' => 'Pas de billet',
 
-    #: config/controller/admin/appdesk.config.php:19
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:22
     'Show all posts' => 'Afficher tous les billets',
 
     #: config/config.php:12
     'Blog' => 'Blog',
 
-    #: config/common/post.config.php:15
+    #: config/common/post.config.php:16
     #: config/metadata.config.php:56
     'Add a post' => 'Ajouter un billet',
 
     #. Crud
-    #: config/common/post.config.php:26
+    #: config/common/post.config.php:20
     'All right, your new post has been added.' => 'Parfait, nouveau billet ajouté.',
 
-    #: config/common/post.config.php:27
+    #: config/common/post.config.php:21
     'The post has been deleted.' => 'Le billet a été supprimé.',
 
     #. General errors
-    #: config/common/post.config.php:30
+    #: config/common/post.config.php:24
     'This post doesn’t exist any more. It has been deleted.' => 'Ce billet n’existe plus. Il a été supprimé.',
 
-    #: config/common/post.config.php:31
+    #: config/common/post.config.php:25
     'We cannot find this post.' => 'Nous n’arrivons pas à trouver ce billet.',
 
     #. Deletion popup
-    #: config/common/post.config.php:38
+    #: config/common/post.config.php:28
     'Deleting the post ‘{{title}}’' => 'Supprimer le billet «&nbsp;{{title}}&nbsp;»',
 
     #. Delete action's labels
-    #: config/common/post.config.php:42
+    #: config/common/post.config.php:31
     'Yes, delete this post' => 'Oui, supprimer ce billet',
 
-    #: config/common/post.config.php:44
+    #: config/common/post.config.php:32
+    'Yes, delete these {{count}} posts' => 'Oui, supprimer ces {{count}} billets',
+
+    #: config/common/post.config.php:34
     '1 post' => '1 billet',
 
-    #: config/common/post.config.php:45
+    #: config/common/post.config.php:35
     '{{count}} posts' => '{{count}} billets',
 
     #. Keep only if the model has the behaviour Contextable
-    #: config/common/post.config.php:48
+    #: config/common/post.config.php:38
     'This post exists in <strong>{{context_count}} contexts</strong>.' => 'Ce billet existe dans <strong>{{context_count}} contextes</strong>.',
 
-    #: config/common/post.config.php:49
+    #: config/common/post.config.php:39
     'This post exists in <strong>{{language_count}} languages</strong>.' => 'Ce billet existe dans <strong>{{language_count}} langues</strong>.',
 
-    #: config/common/category.config.php:14
+    #: config/common/category.config.php:15
     'Add a category' => 'Ajouter une catégorie',
 
 );
