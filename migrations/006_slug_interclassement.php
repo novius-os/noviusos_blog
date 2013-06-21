1bc811a206423c93bd37a5f3bf52e4d41a71f0fe
Gilles FELIX <felix@novius.com>
2013-06-21 11:45:29 +0200

    "Bugfix : Change interclassement on all columns use like a slug"

---
+++ migrations/006_slug_interclassement.php
@@ -0,0 +1,6 @@
+<?php
+namespace Nos\BlogNews\Blog\Migrations;
+
+class Slug_Interclassement extends \Nos\Migration
+{
+}
---
+++ migrations/006_slug_interclassement.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `nos_blog_post` CHANGE `post_virtual_name` `post_virtual_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
+ALTER TABLE `nos_blog_category` CHANGE `cat_virtual_name` `cat_virtual_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL ;
