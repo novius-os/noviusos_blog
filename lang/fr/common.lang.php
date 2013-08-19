98e7221af2c5baeb172a45af11c9b1b936401a88
Julian Esperat <esperat@novius.fr>
2013-08-19 18:58:45 +0200

    "Updating translations"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,6 +1,6 @@
 <?php
 
-// Generated on 09/08/2013 10:16:42
+// Generated on 19/08/2013 18:12:14
 
 // 20 out of 20 messages are translated (100%).
 // 80 out of 80 words are translated (100%).
--- lang/ja/common.lang.php
+++ lang/ja/common.lang.php
@@ -1,6 +1,6 @@
 <?php
 
-// Generated on 05/07/2013 14:57:58
+// Generated on 19/08/2013 18:12:14
 
 // 20 out of 20 messages are translated (100%).
 // 80 out of 80 words are translated (100%).
