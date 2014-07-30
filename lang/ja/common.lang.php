51f8bbb500a1eb33d0838944a0c8f2252b112c01
foine <brousse@novius.fr>
2014-07-30 10:07:32 +0200

    "Japanese translation"

--- lang/ja/common.lang.php
+++ lang/ja/common.lang.php
@@ -1,9 +1,9 @@
 <?php
 
-// Generated on 11/12/2013 17:57:29
+// Generated on 30/07/2014 09:57:01
 
-// 17 out of 17 messages are translated (100%).
-// 70 out of 70 words are translated (100%).
+// 29 out of 29 messages are translated (100%).
+// 115 out of 115 words are translated (100%).
 
 return array(
     #: config/controller/admin/appdesk.config.php:16
@@ -30,12 +30,12 @@ return array(
     #: config/controller/admin/appdesk.config.php:28
     'Show all posts' => '全投稿を表示',
 
-    #: config/config.php:12
-    'Blog' => 'ブログ',
-
+    #: config/metadata.config.php:70
     #: config/common/post.config.php:15
-    #: config/metadata.config.php:69
     'Add a post' => '新規投稿',
+
+    #: config/common/category.config.php:15
+    'Add a category' => 'カテゴリーを追加',
 
     #. Crud
     #: config/common/post.config.php:19
@@ -70,7 +70,43 @@ return array(
         0 => 'この投稿は <strong>{{language_count}} つの言語</strong>にあります。',
     ),
 
-    #: config/common/category.config.php:15
-    'Add a category' => 'カテゴリーを追加',
+    #: config/menu/item/category.config.php:14
+    'Add a blog category link' => 'カテゴリーリンクを追加',
+
+    #: config/menu/item/category.config.php:15
+    'New blog category link' => '新しいカテゴリーリンク',
+
+    #: config/menu/item/category.config.php:46
+    'Blog category:' => 'カテゴリー: ',
+
+    #: config/menu/item/category.config.php:57
+    'No blog category selected' => 'カテゴリーが選択されていません',
+
+    #: config/menu/item/category.config.php:58
+    'Pick a blog category' => 'カテゴリーを選択',
+
+    #: config/menu/item/category.config.php:59
+    'Pick another blog category' => '別のカテゴリーを選択',
+
+    #: config/menu/item/category.config.php:60
+    'Un-select this blog category' => 'カテゴリーを外す',
+
+    #: config/menu/item/post.config.php:46
+    'Blog post:' => 'ブログ投稿: ',
+
+    #: config/menu/item/post.config.php:57
+    'No blog post selected' => 'ブログ投稿が選択されていません',
+
+    #: config/menu/item/post.config.php:58
+    'Pick a blog post' => 'ブログ投稿を選択',
+
+    #: config/menu/item/post.config.php:59
+    'Pick another blog post' => '別のブログ投稿を選択',
+
+    #: config/menu/item/post.config.php:60
+    'Un-select this blog post' => 'ブログ投稿を外す',
+
+    #: config/config.php:12
+    'Blog' => 'ブログ',
 
 );
